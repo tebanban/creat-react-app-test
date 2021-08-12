79de567ce99b56e3bafe1d33c3673b3567c89e68
tebanban <estebancalf@gmail.com>
2021-08-12 15:46:40 +0000

    navbar

---
+++ src/router/AppRouter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {  BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AboutPage from '../pages/AboutPage';
+import ContactPage from '../pages/ContactPage';
+import Error404 from '../pages/Error404';
+import HomePage from '../pages/HomePage';
+import Navbar from '../components/Navbar';
+
+export default function AppRouter() {
+    return (
+        
+        <Router>
+            <Navbar  />
+            <Switch>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+                <Route exact path="/about">
+                    <AboutPage />
+                </Route>  
+                <Route exact path="/contact">
+                    <ContactPage />
+                </Route> 
+                <Route path="*">
+                    <Error404 />
+                </Route>   
+            </Switch>
+        </Router>
+    )
+}
