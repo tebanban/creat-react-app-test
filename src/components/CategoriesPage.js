f9d588a36f2f31fb11b3fdb081ca0bbff8ba5051
tebanban <estebancalf@gmail.com>
2021-08-13 21:18:37 +0000

    skip

--- src/components/CategoriesPage.js
+++ src/components/CategoriesPage.js
@@ -1,21 +1,30 @@
 import React from 'react'
-import { useLocation } from 'react-router'
+import { useLocation, useHistory } from 'react-router'
 
 export default function CategoriesPage() {
     const location = useLocation();
+    const history = useHistory();
+
     const query = new URLSearchParams(location.search);
-    const skip = query.get("skip") || 0;
-    const limit = query.get("limit") || 10
+    const skip = parseInt(query.get("skip") || 0)
+    const limit =parseInt (query.get("limit") || 10)
+    const handleClick= () => {
+     
+        query.set("skip", skip + limit)
+       // query.set("limit",200)
+        history.push({search:query.toString()})
+
+    }
 
     
-
-    console.log(query);
 
     return (
         <div>
             <h1>Categories Page</h1>
             <h5>Skip = {skip}</h5>
             <h5>Limit = {limit}</h5>
+
+            <button onClick={handleClick}>Next</button>
         </div>
     )
 }
